1bdac2838defe79534249d3c374106e1840cbd77
ASIMONARD <ar.simonard.com>
2024-12-10 10:30:55 +0100

    Challenge 10 about JS

---
+++ challenge 10.js
@@ -0,0 +1,10 @@
+console.log("Teste 1 :")
+let bill1 = 150
+let tip1 = (bill1 >= 50 && bill1 <= 300) ? (bill1 * 15 / 100) : (bill * 20 / 100)
+console.log("La note était de " + bill1 + " le pourboire de, " + tip1 + " et la valeur total était de " + (bill1 + tip1))
+console.log("")
+
+console.log("Teste 2 :")
+let bill2 = 400
+let tip2 = (bill2 >= 50 && bill2 <= 300) ? (bill2 * 15 / 100) : (bill2 * 20 / 100)
+console.log("La note était de " + bill2 + " le pourboire de, " + tip2 + " et la valeur total était de " + (bill2 + tip2))
